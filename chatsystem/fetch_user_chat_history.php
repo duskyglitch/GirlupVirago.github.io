362eab1d43850d932ad9c05a5bfb61a161ba10a4
duskyglitch <64149165+duskyglitch@users.noreply.github.com>
2021-03-15 15:52:17 +0530

    Add files via upload

---
+++ chatsystem/fetch_user_chat_history.php
@@ -0,0 +1,12 @@
+<?php
+
+//fetch_user_chat_history.php
+
+include('databaseconn.php');
+
+session_start();
+
+echo fetch_user_chat_history($_SESSION['user_id'], $_POST['to_user_id'], $connect);
+
+?>
+
